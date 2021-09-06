15774933517871dfa7cb5df7cc83219c159a2014
Andre Rodriguez <andre.rodriguez9722@outlook.com>
2021-09-06 19:54:00 -0400

    Added rough implementation for main. Fetched jinja templates & json data to serve a static html page to given output directory.

--- insta485generator/__main__.py
+++ insta485generator/__main__.py
@@ -1,10 +1,49 @@
+import click
+import os
+import json
+from jinja2 import Environment, PackageLoader, select_autoescape, Template
+
 """Build static HTML site from directory of HTML templates and plain files."""
+#    Implement the following in main:
+# 1. Handle command line arguments using the click library
+# 2. Render the provided templates and write them to output 
+#    files and create all the necessary directories and files needed to do this
+# 3. Copy over a static directory if it is provided
+# 4. Handle errors
+#@click.command(context_settings=dict(help_option_names=['-h', '--help']))
 
+@click.command()
+@click.argument('INPUT_DIR', required=True, type=click.Path(exists=True))
+@click.option('-o', '--output', 'PATH', help="Output directory")
+@click.option('-v', '--verbose', help="Print more output")
 
-def main():
-    """Top level command line interface."""
-    print("Hello world!")
-
+def main(PATH, verbose, input_dir):
+    """Fetch JSON, URL, & JINJA from input_dir"""
+    click.echo(input_dir)
+    print(PATH)
+    #load the json data:
+    jsonFile = open(input_dir + "/" + "config.json", "r")
+    data = json.load(jsonFile)
+    print(data)
+    #load the jinja template:
+    env = Environment(
+    loader=PackageLoader(input_dir),
+    autoescape=select_autoescape(['html', 'xml']))
+    template = env.get_template("index.html")
+    #print(template.render(the="variables", go="here"))
+    #Extract the url from the json dictionary:
+    #Not sure when we are suppose to cut off the forward slash..
+    url = data[0]["url"]
+    #print(url)
+    """Serve JSON & JINJA, to ${input_dir}/url """
+    target_path = input_dir + '/html' if PATH == None else PATH
+    print(target_path)
+    #Store the jinja Template in the target_path:
+    htmlOutput = template.render(data[0].get("context"))
+    #print(htmlOutput)
+    os.mkdir(target_path)
+    with open(target_path + "/index.html", "w") as file:
+        file.write(htmlOutput)
 
 if __name__ == "__main__":
     main()
